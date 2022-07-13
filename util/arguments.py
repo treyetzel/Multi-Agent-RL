e2d4d59c2c0a0cb01dac5e9770b2b3fc1bc6386b
Kevin <kevduong@ksu.edu>
2022-07-13 14:06:50 -0500

    test

--- util/arguments.py
+++ util/arguments.py
@@ -1,68 +1,50 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='RL')
+parser = argparse.ArgumentParser(description="RL")
 
 # Training loop parameters
 parser.add_argument(
-    '--max_steps',
-    type=int,
-    default=200000,
-    help='max steps to train models on')
+    "--max_steps", type=int, default=10000, help="max steps to train models on"
+)
+
+parser.add_argument("--log_steps", type=int, default=250, help="Number of steps to log")
 
 parser.add_argument(
-    '--log_steps',
+    "--num_envs",
+    type=int,
+    default=12,
+    help="Number of parallel environments to collect experiences from",
+)
+
+parser.add_argument(
+    "--warm_up_steps",
     type=int,
     default=2000,
-    help='Number of steps to log')
+    help="Number of steps to explore before training",
+)
 
 parser.add_argument(
-    '--num_envs',
-    type=int,
-    default=24,
-    help='Number of parallel environments to collect experiences from')
-
-parser.add_argument(
-    '--warm_up_steps',
-    type=int,
-    default=2000,
-    help='Number of steps to explore before training')
-
-parser.add_argument(
-    '--k_step',
-    type=int,
-    default=4,
-    help='update main networks every k step')
+    "--k_step", type=int, default=4, help="update main networks every k step"
+)
 
 # IDQN parameters
-parser.add_argument(
-    '--lr',
-    type=float,
-    default=5e-4,
-    help='learning rate')
+parser.add_argument("--lr", type=float, default=5e-4, help="learning rate")
 
 parser.add_argument(
-    '--buffer_size',
-    type=int,
-    default=10000,
-    help='buffer size per agent')
+    "--buffer_size", type=int, default=10000, help="buffer size per agent"
+)
 
 parser.add_argument(
-    '--batch_size',
+    "--batch_size",
     type=int,
     default=32,
-    help="batch size to sample from replay buffers"
+    help="batch size to sample from replay buffers",
 )
 
 parser.add_argument(
-    '--num_updates',
-    type=int,
-    default=5,
-    help="Number of times to update on new batches"
+    "--num_updates", type=int, default=5, help="Number of times to update on batches"
 )
 
 parser.add_argument(
-    '--gamma',
-    type=float,
-    default=0.99,
-    help="Gamma for discounting expected rewards"
+    "--gamma", type=float, default=0.99, help="Gamma for discounting expected rewards"
 )
